d949ed8f1c810fd0145ea78cc9470b589a3fc3a2
Kevin <websterguy@gmail.com>
2020-02-18 12:47:19 -0800

    Implement add task

--- app/src/main/java/edu/miracosta/cs134/kwebb/todo2day/model/DBHelper.java
+++ app/src/main/java/edu/miracosta/cs134/kwebb/todo2day/model/DBHelper.java
@@ -1,8 +1,10 @@
 package edu.miracosta.cs134.kwebb.todo2day.model;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -20,8 +22,27 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String FIELD_DESCRIPTION = "description";
     public static final String FIELD_IS_DONE = "is_done";
 
+    /**
+     * Adds a new task to the database
+     * @param task The new task to be added
+     */
     public void addTask(Task task) {
+        // Decide whether we are reading or writing to/from DB
+        // For adding tasks, we're writing to DB
+        SQLiteDatabase db = getWritableDatabase();
 
+        // When we add tasks, use data structure ContentValues (key, value) pairs
+        ContentValues values = new ContentValues();
+
+        // Set up key/value pairs
+        values.put(FIELD_DESCRIPTION, task.getDescription());
+        values.put(FIELD_IS_DONE, task.isDone() ? 1 : 0);
+
+        // Insert data
+        db.insert(TABLE_NAME, null, values);
+
+        // Close connection to database
+        db.close();
     }
 
     public List<Task> getAllTasks() {
@@ -47,12 +68,26 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        String createSQL = "CREATE TABLE IF NOT EXISTS "
+                + TABLE_NAME + "("
+                + KEY_FIELD_ID + " INTEGER PRIMARY KEY, "
+                + FIELD_DESCRIPTION + " TEXT, "
+                + FIELD_IS_DONE + " INTEGER" + ")";
 
+        Log.i(DATABASE_NAME, createSQL);
+        db.execSQL(createSQL);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop old table, recreate the new
+        if (newVersion > oldVersion) {
+            String dropSQL = "DROP TABLE IF EXISTS " + TABLE_NAME;
+            Log.i(DATABASE_NAME, dropSQL);
+            db.execSQL(dropSQL);
 
+            onCreate(db);
+        }
     }
 }
